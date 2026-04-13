ee43a16c1fbdb4f1ef4273c4e99a0461eb1300a3
abdelkadert95 <abdelkadert95@gmail.com>
2026-04-13 02:00:37 +0100

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 
@@ -16,12 +17,28 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark scroll-smooth">
       <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-68PEBHGKLD"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-68PEBHGKLD');
+          `}
+        </Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&family=Inter:wght@300;400;500;600;700&family=Source+Code+Pro:wght@400;500&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body bg-background text-foreground antialiased selection:bg-accent/30 selection:text-accent">
-        {children}
+        <main>
+          {children}
+        </main>
         <Toaster />
       </body>
     </html>
